310b16b5d716d8deb9081caa17ffde71f45f45f2
Reena Thomas <reenatt@yahoo.com>
2021-12-28 12:34:14 -0800

    Connect to database

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/poems", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// shortcut to mongoose.connection object
+const db = mongoose.connection;
+
+db.on('connected', function () {
+  console.log(`Connected to ${db.name} at ${db.host}:${db.port}`);
+});
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var methodOverride = require('method-override');
+require('dotenv').config();
+require('./config/database');
 
 var app = express();
 
@@ -17,6 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
